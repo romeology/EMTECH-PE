b2b334c22d462fd6b9c5abea2efbb8eb069904a8
Juuliano03 <gio2.cabanero@gmail.com>
2021-05-02 19:45:25 +0800

    1 file modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -6,9 +6,21 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+Met: number;
+CalBurn: number;
+kg: number;
+mins: number;
+kglost: number;
 
-  constructor(public navCtrl: NavController) {
+CalculateCalBurn(exercise) {
+this.Met = exercise;
+this.CalBurn=(this.Met * 3.5 * (this.kg/200)) *this.mins;
+this.CalBurn = parseFloat(this.CalBurn.toFixed(2));
+this.kglost = this.CalBurn / 7700;
+this.kglost = parseFloat(this.kglost.toFixed(2));
 
-  }
+   
 
+  
+}
 }
